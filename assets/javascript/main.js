28df109eb31432ddfd2facc10e686546e14f0f4f
zrh231 <zrh231@gmail.com>
2018-12-06 14:04:58 -0500

    added files

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -35,48 +35,47 @@ function displayGifInfo() {
         // Appending the image
         gifDiv.append(image);
         
-        // Putting the entire gif above the previous movies
+        // Putting the entire gif above the previous gifs
         $("#gif-view").prepend(gifDiv);
     
     });
 
 }
 
-// Function for displaying movie data
+// Function for displaying gif data
 function renderButtons() {
 
-// Deleting the movies prior to adding new movies
-// (this is necessary otherwise you will have repeat buttons)
-$("#buttons-view").empty();
 
-// Looping through the array of gifs
-for (var i = 0; i < topics.length; i++) {
+    $("#buttons-view").empty();
 
- // Then dynamicaly generating buttons for each gif in the array
- // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
- var a = $("<button>");
- // Adding a class of gif-btn to our button
- a.addClass("gif-btn");
- // Adding a data-attribute
- a.attr("data-name", topics[i]);
- // Providing the initial button text
- a.text(topics[i]);
- // Adding the button to the buttons-view div
- $("#buttons-view").append(a);
-}
+    // Looping through the array of gifs
+    for (var i = 0; i < topics.length; i++) {
+
+    // Then dynamicaly generating buttons for each gif in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of gif-btn to our button
+    a.addClass("gif-btn");
+    // Adding a data-attribute
+    a.attr("data-name", topics[i]);
+    // Providing the initial button text
+    a.text(topics[i]);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+    }
 }
 
 // This function handles events where a gif button is clicked
 $("#add-gif").on("click", function(event) {
-event.preventDefault();
-// This line grabs the input from the textbox
-var gif = $("#gif-input").val().trim();
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var gif = $("#gif-input").val().trim();
 
-// Adding gif from the textbox to our array
-topics.push(gif);
+    // Adding gif from the textbox to our array
+    topics.push(gif);
 
-// Calling renderButtons which handles the processing of our gif array
-renderButtons();
+    // Calling renderButtons which handles the processing of our gif array
+    renderButtons();
 });
 
 // Adding a click event listener to all elements with a class of "gif-btn"
